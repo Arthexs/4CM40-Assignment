e72ebe989aa2f57340f92806098c0de4ec04d2a8
Arthex <116348796+Arthexs@users.noreply.github.com>
2024-02-21 16:15:31 +0100

    array for disturbances

--- Exercise1.m
+++ Exercise1.m
@@ -7,13 +7,17 @@ Av_star = 0.001;
 rho = 980;
 K = 0.01;
 phi_o_star = 0.001;
+Av_disturbance = 10^(-5);
 
 StopTime = 20000;
 TimeStep = 1;
 
-phi_i.time = [0:TimeStep:StopTime]';
+phi_i.time = (0:TimeStep:StopTime)';
 phi_i.signals.values = [phi_o_star*ones(1,2000),...
     phi_o_star/2*ones(1,10000),phi_o_star*ones(1,StopTime-12000+1)]';
+
+Av.time = (0:TimeStep:StopTime)';
+Av.signals.values = generate_array(StopTime, TimeStep, Av_star, Av_disturbance);
 
 %% Run model
 model = 'Model1.slx';
@@ -46,3 +50,39 @@ plot(SimOut.p.Time,SimOut.p.Data)
 title('Pressure over time')
 xlabel('Time [s]')
 ylabel('Pressure [Pa]')
+
+%% Functions
+% Needs fixing
+function array = generate_array(stoptime, stepsize, Av_star, Av_disturbance)
+    % Create time vector
+    time = (0:stepsize:stoptime)';
+
+    % Initialize array
+    array = Av_star*ones(size(time));
+
+    % Determine cut off at periodic block signal
+    period_cut_off = 3000/stepsize;
+
+    % Determine the period length for the periodic signal
+    period_length = 6000/stepsize;
+    total_vals = stoptime*stepsize;
+    full_periods = total_vals/period_length;
+    for i = 1:length(array)
+        if i < period_cut_off
+            continue
+        end
+        if 
+    end
+
+
+    % Determine the start index of the periodic signal
+    start_index_periodic_signal = find(time >= 1000, 1);
+
+    % Generate the periodic block-pulse signal
+    for i = 1:period_length:numel(time)
+        end_index = min(i+period_length-1, numel(time));
+        array(start_index_periodic_signal:end_index) = ...
+            Av_star + Av_disturbance * mod(i, 2) - Av_disturbance * (1 - mod(i, 2));
+    end
+end
+
