0255634f64f39663e6569030af3b43a4daf51a9c
Arthex <116348796+Arthexs@users.noreply.github.com>
2024-02-23 12:13:29 +0100

    Finalized images for report

--- Exercise1.m
+++ Exercise1.m
@@ -34,7 +34,7 @@ subplot(3,2,1)
 plot(Time,phi_i_in)
 title('Inflow over time')
 xlabel('Time [s]')
-ylabel('Volumetric flow [m\^3/s]')
+ylabel('Volumetric flow [m^3/s]')
 
 subplot(3,2,2)
 plot(SimOut.h.Time,SimOut.h.Data)
@@ -46,7 +46,7 @@ subplot(3,2,3)
 plot(SimOut.phi_o.Time,SimOut.phi_o.Data)
 title('Outflow over time')
 xlabel('Time [s]')
-ylabel('Volumetric flow [m\^3/s]')
+ylabel('Volumetric flow [m^3/s]')
 
 subplot(3,2,4)
 plot(SimOut.p.Time,SimOut.p.Data)
@@ -60,6 +60,13 @@ title('Mass over time')
 xlabel('Time [s]')
 ylabel('Mass [kg]')
 
+subplot(3,2,6)
+plot(Time,Av_in)
+title('A_v over time')
+xlabel('Time [s]')
+ylabel('A_v [m^2]')
+
+%{
 % The non-linear disturbance model
 figure, 
 sgtitle('The non-linear with disturbance model')
@@ -67,7 +74,7 @@ subplot(3,2,1)
 plot(Time,phi_i_in)
 title('Inflow over time')
 xlabel('Time [s]')
-ylabel('Volumetric flow [m\^3/s]')
+ylabel('Volumetric flow [m^3/s]')
 
 subplot(3,2,2)
 plot(SimOut.h1.Time,SimOut.h1.Data)
@@ -79,7 +86,7 @@ subplot(3,2,3)
 plot(SimOut.phi_o1.Time,SimOut.phi_o1.Data)
 title('Outflow over time')
 xlabel('Time [s]')
-ylabel('Volumetric flow [m\^3/s]')
+ylabel('Volumetric flow [m^3/s]')
 
 subplot(3,2,4)
 plot(SimOut.p1.Time,SimOut.p1.Data)
@@ -93,42 +100,67 @@ title('Mass over time')
 xlabel('Time [s]')
 ylabel('Mass [kg]')
 
-
+%}
 % Linearized delta model
 % x = m, y = [p;phi_o;h]
 
 figure, 
-sgtitle('The delta linear model')
+sgtitle('The change in linear and non-linear model')
 subplot(3,2,1)
+hold on
 plot(Time,phi_i_in)
+hold off
 title('Inflow over time')
 xlabel('Time [s]')
-ylabel('Volumetric flow [m\^3/s]')
+ylabel('Volumetric flow [m^3/s]')
 
 subplot(3,2,2)
-plot(SimOut.y.Time,SimOut.y.Data(3,:))
+hold on
+plot(SimOut.y.Time,SimOut.y.Data(3,:),'DisplayName','Linear')
+plot(Time,SimOut.h1.Data-SimOut.h.Data,'DisplayName','Non-linear')
+hold off
 title('Height over time')
 xlabel('Time [s]')
 ylabel('\Delta Height [m]')
+legend
 
 subplot(3,2,3)
-plot(SimOut.y.Time,SimOut.y.Data(2,:))
+hold on
+plot(SimOut.y.Time,SimOut.y.Data(2,:),'DisplayName','Linear')
+plot(Time,SimOut.phi_o1.Data-SimOut.phi_o.Data,'DisplayName','Non-linear')
+hold off
 title('Outflow over time')
 xlabel('Time [s]')
-ylabel('\Delta Volumetric flow [m\^3/s]')
+ylabel('\Delta Volumetric flow [m^3/s]')
+legend
 
 subplot(3,2,4)
-plot(SimOut.y.Time,SimOut.y.Data(1,:))
+hold on
+plot(SimOut.y.Time,SimOut.y.Data(1,:),'DisplayName','Linear')
+plot(Time,SimOut.p1.Data-SimOut.p.Data,'DisplayName','Non-linear')
+hold off
 title('Pressure over time')
 xlabel('Time [s]')
 ylabel('\Delta Pressure [Pa]')
+legend
 
 subplot(3,2,5)
-plot(SimOut.x.Time,SimOut.x.Data)
+hold on
+plot(SimOut.x.Time,SimOut.x.Data,'DisplayName','Linear')
+plot(Time,SimOut.m1.Data-SimOut.m.Data,'DisplayName','Non-linear')
+hold off
 title('Mass over time')
 xlabel('Time [s]')
 ylabel('\Delta Mass [kg]')
+legend
 
+subplot(3,2,6)
+plot(Time,Av_in-Av_star)
+title('Disturbances over time')
+xlabel('Time [s]')
+ylabel('A_v disturbance [m^2]')
+
+%{
 % Linear model 
 figure, 
 sgtitle('The linear model')
@@ -136,7 +168,7 @@ subplot(3,2,1)
 plot(Time,phi_i_in)
 title('Inflow over time')
 xlabel('Time [s]')
-ylabel('Volumetric flow [m\^3/s]')
+ylabel('Volumetric flow [m^3/s]')
 
 subplot(3,2,2)
 plot(SimOut.y.Time,SimOut.y.Data(3,:)+SimOut.h.Data')
@@ -148,7 +180,7 @@ subplot(3,2,3)
 plot(SimOut.y.Time,SimOut.y.Data(2,:)+SimOut.phi_o.Data')
 title('Outflow over time')
 xlabel('Time [s]')
-ylabel('Volumetric flow [m\^3/s]')
+ylabel('Volumetric flow [m^3/s]')
 
 subplot(3,2,4)
 plot(SimOut.y.Time,SimOut.y.Data(1,:)+SimOut.p.Data')
@@ -161,7 +193,7 @@ plot(SimOut.x.Time,SimOut.x.Data+SimOut.m.Data)
 title('Mass over time')
 xlabel('Time [s]')
 ylabel('Mass [kg]')
-
+%}
 
 %% Functions
 % Needs fixing
