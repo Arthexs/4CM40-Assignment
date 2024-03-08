686c69a7d27f906aae5b827bfe8868f341ccdb1e
Thijs Beurskens <t.m.h.beurskens@student.tue.nl>
2024-03-08 12:11:16 +0100

    add ode45 solver

--- exercise 2/exercise2.m
+++ exercise 2/exercise2.m
@@ -1,6 +1,7 @@
 clear, clc, close all
 
 %%
+global A in
 f = [0, 0.3, 0.3, 0.1, 0.1, 0.1, 0];
 N = 6;
 v = 1;
@@ -16,3 +17,15 @@ A = [-forward(1)-backward(1), backward(2), 0, 0, 0, 0;
      0, 0, forward(3), -forward(4)-backward(4), backward(5), 0;
      0, 0, 0, forward(4), -forward(5)-backward(5), backward(6);
      0, 0, 0, 0, forward(5), -forward(6)-backward(6), ]
+
+syms c1 c2 c3 c4 c5 c6
+x = [c1,c2,c3,c4,c5,c6].'
+in = [v*N/L,0,0,0,0,0].';
+out = [0,0,0,0,0,1]*x;
+
+ode45(@func,[1:10],zeros(size(x)))
+
+function dx_dt = func(t,x)
+     global A in
+     dx_dt = A*x+in;
+end
