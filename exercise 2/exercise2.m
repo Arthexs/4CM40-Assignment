8ac1411b827af00d706db38ef897c57039cdcbcb
Thijs Beurskens <t.m.h.beurskens@student.tue.nl>
2024-03-13 16:41:43 +0100

    compute average residence time

--- exercise 2/exercise2.m
+++ exercise 2/exercise2.m
@@ -11,20 +11,17 @@ phi = v*A;
 syms c1 c2 c3 c4 c5 c6
 x = [c1,c2,c3,c4,c5,c6].';
 in = [v*N/L,0,0,0,0,0].';
-out = [0,0,0,0,0,1]*x;
+out = [0,0,0,0,0,1].';
 start = 0;
-finish = 2.5;
-stepsize = 0.001;
+finish = 3.5;
+stepsize = 0.0001;
 
 
 figure
 hold on
-for i = 1:2
-     if (i == 1)
-          f = [0, 0.3, 0.3, 0.1, 0.1, 0.1, 0];
-     else
-          f = [0, 0,   0,   0,   0,   0,   0];
-     end
+for i = 0:1
+     f = [0, 0.3, 0.3, 0.1, 0.1, 0.1, 0] * i;
+     
      forward = (f(2:N+1)+1)*v*N/L;
      backward = f(1:N)*v*N/L;
 
@@ -36,10 +33,20 @@ for i = 1:2
           0, 0, 0, 0, forward(5), -forward(6)-backward(6), ]
 
      [t,c] = ode45(@(t,x) func(t,x,A,in,stepsize,start), start:stepsize:finish,zeros(size(x)));
-
-     plot(t,c);
+     y = c*out;
+     trapz(y,t);
+     m_1 = 0;
+     for i = 1:length(y)
+          m_1 = m_1 + t(i)*y(i)*stepsize;
+     end
+     y = y / m_1;
+     
+     plot(t,y);
+     
 end
-
+xlabel("time(s)")
+ylabel("concentration")
+legend(["no backflow","regular backflow"])
 
 function dydx = func(t,x,A,in,stepsize,start)
      if (t < start+stepsize)
