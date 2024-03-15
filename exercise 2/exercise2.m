7d34ca5c3638b66e73c420e54f89e30e963a6796
Thijs Beurskens <t.m.h.beurskens@student.tue.nl>
2024-03-15 12:34:00 +0100

    restructuring

--- exercise 2/exercise2.m
+++ exercise 2/exercise2.m
@@ -6,12 +6,12 @@ N = 6;
 v = 1;
 A = 1;
 L = 1;
-phi = v*A;
+phi = v*N/L;
 
 syms c1 c2 c3 c4 c5 c6
 x = [c1,c2,c3,c4,c5,c6].';
-in = [v*N/L,0,0,0,0,0].';
-out = [0,0,0,0,0,1].';
+in = [phi,0,0,0,0,0].';
+out = [0,0,0,0,0,phi].';
 start = 0;
 finish = 3.5;
 stepsize = 0.0001;
@@ -21,24 +21,24 @@ figure
 hold on
 for i = 0:1
      if (i == 0)
-          fprintf("no backflow case:\n")
+          fprintf("no backflow case:")
      else
-          fprintf("backflow case:\n")
+          fprintf("backflow case:")
      end
      f = [0, 0.3, 0.3, 0.1, 0.1, 0.1, 0] * i;
      
-     forward = (f(2:N+1)+1)*v*N/L;
-     backward = f(1:N)*v*N/L;
+     forward = (f(2:N+1)+1)*phi;
+     backward = f(1:N)*phi;
 
      A = [-forward(1)-backward(1), backward(2), 0, 0, 0, 0;
           forward(1), -forward(2)-backward(2), backward(3), 0, 0, 0;
           0, forward(2), -forward(3)-backward(3), backward(4), 0, 0;
           0, 0, forward(3), -forward(4)-backward(4), backward(5), 0;
           0, 0, 0, forward(4), -forward(5)-backward(5), backward(6);
-          0, 0, 0, 0, forward(5), -forward(6)-backward(6), ];
+          0, 0, 0, 0, forward(5), -forward(6)-backward(6), ]
 
      [t,c] = ode45(@(t,x) func(t,x,A,in,stepsize,start), start:stepsize:finish,zeros(size(x)));
-     y = c*out;
+     y = c*out/(phi);
      %normalize area
      y = y / trapz(t,y,1);
      m_1 = 0;
@@ -46,8 +46,10 @@ for i = 0:1
           m_1 = m_1 + t(j)*y(j)*stepsize;
      end
      fprintf("area = " + trapz(t,y,1) + ", m_1 = " + m_1)
+     %a) & b)
      plot(t,y);
 
+     %c)
      mu = zeros(size(1:3));
      for j = 1:3
           for k = 1:length(y)
@@ -56,6 +58,18 @@ for i = 0:1
      end
      mu
      
+     %d)
+     %check positive definite:
+     flow_mat = [forward(1)+backward(1), -backward(2), 0, 0, 0, 0;
+                 -forward(1), forward(2)+backward(2), -backward(3), 0, 0, 0;
+                 0, -forward(2), forward(3)+backward(3), -backward(4), 0, 0;
+                 0, 0, -forward(3), forward(4)+backward(4), -backward(5), 0;
+                 0, 0, 0, -forward(4), forward(5)+backward(5), -backward(6);
+                 0, 0, 0, 0, -forward(5), forward(6)+backward(6), ]
+     
+     %check stability of system
+     eig(A)
+
 end
 xlabel("time(s)")
 ylabel("concentration")
